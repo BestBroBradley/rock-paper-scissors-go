f3ea141c12d06b428cad5f5e53fa90d8c69da1db
Allison Bradley <allison.bradley@outlook.com>
2019-12-14 14:17:18 -0700

    Added computer generated guess functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var images = ["assets/rock.png", "assets/paper.png", "assets/scissors.png"];
-var nextBtn =  document.querySelector(".next")
+var nextBtn = document.querySelector(".next")
 var prevBtn = document.querySelector(".prev")
 var carousel = document.querySelector(".carouselbox")
 var imgIndex = 0
@@ -11,6 +11,12 @@ var losses = document.querySelector("#losses")
 var yourImage = document.querySelector("#yourImage")
 var compImage = document.querySelector("#compImage")
 var compCounter = document.querySelector("#compCounter")
+var yourImageBox = document.querySelector("#yourImageBox")
+var compImageBox = document.querySelector("#compImageBox")
+
+var p = "assets/papers.png"
+var s = "assets/scissors.png"
+var r = "assets/rock.png"
 
 var winCount = 0
 var tieCount = 0
@@ -22,7 +28,7 @@ ties.textContent = `Ties:${tieCount}`
 
 img.src = images[0];
 
-prevBtn.addEventListener("click", function(event) {
+prevBtn.addEventListener("click", function (event) {
     event.preventDefault();
     imgIndex--
     if (imgIndex === -1) {
@@ -30,9 +36,9 @@ prevBtn.addEventListener("click", function(event) {
     }
     img.src = images[imgIndex];
     // writing this as a template literal instead = carousel.style.backgroundImage = `url(${images[imgIndex]})`;
-} )
+})
 
-nextBtn.addEventListener("click", function(event) {
+nextBtn.addEventListener("click", function (event) {
     event.preventDefault();
     imgIndex++
     if (imgIndex === images.length) {
@@ -40,14 +46,56 @@ nextBtn.addEventListener("click", function(event) {
     }
     img.src = images[imgIndex];
     // writing this as a template literal instead = carousel.style.backgroundImage = `url(${images[imgIndex]})`;
-} )
+})
 
-submitBtn.addEventListener("click", function(event){
+submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
     var userGuess = images[imgIndex]
-    console.log(userGuess)
     yourImage.src = userGuess
-    compCounter.setAttribute ("class", "show")
-    console.log(compCounter)
+    yourImageBox.setAttribute("class", "imgDiv")
+    compCounter.setAttribute("class", "show")
+    compImageBox.setAttribute("class", "hidden")
+    var secondsLeft = 3;
+    compCounter.textContent = `...${secondsLeft}`;
+    setTime();
+    
 
-})
+    function setTime() {
+        var waitFor = setInterval(function () {
+            secondsLeft--;
+            compCounter.textContent = `...${secondsLeft}`;
+
+            if (secondsLeft === 0) {
+                clearInterval(waitFor)
+                compCounter.setAttribute("class", "hidden")
+                secondsLeft = 4;
+                showComp();
+
+            }
+        }, 1000);
+
+        function showComp() {
+            var compGuess = images[Math.floor(Math.random() * images.length)];
+            compImageBox.setAttribute("class", "imgDiv")
+            compImage.src = compGuess
+            var showCompTime = 5;
+
+            var waitAgain = setInterval(function () {
+                showCompTime--;
+
+                if (showCompTime === 0) {
+                    clearInterval(waitAgain)
+                    showCompTime = 5;
+                    showResult();
+
+                }
+            }, 1000);
+
+            // function showResult()
+            // if (compGuess === userGuess) {
+                
+            // }
+        }
+
+    }
+} )
